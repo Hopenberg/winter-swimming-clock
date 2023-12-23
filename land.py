c6812b8c8c3594d23abb68f7e70f9d29b17eb6d0
Hopenberg <marcinhopa54@gmail.com>
2023-12-23 16:58:55 +0100

    Pierwsza działająca wersja części lądowej

--- land.py
+++ land.py
@@ -1,42 +1,74 @@
 import time
 from guizero import App, Text, TextBox, PushButton, Slider, Picture
 
-def say_my_name():
-    welcome_message.value = my_name.value
-
-def change_text_size(slider_value):
-    welcome_message.size = slider_value
+def empty():
+    return 0
 
 def secondsToText(seconds):
     minutes = str(int(seconds / 60))
     secondsPart = str(seconds % 60)
 
-    if len(str(secondsPart)) == 1:
+    if len(secondsPart) == 1:
         secondsPart = "0" + secondsPart
+    
+    if (len(minutes) == 1):
+        minutes = "0" + minutes
 
-    return "0" + minutes + ":" + secondsPart
+    return minutes + ":" + secondsPart
 
 def textToSeconds(text):
     [minutes, seconds] = text.split(":")
+
+    print([minutes, seconds])
     
-    return minutes * 60 + seconds
+    return int(minutes) * 60 + int(seconds)
 
-def incrementTime(timer):
-    seconds = textToSeconds(welcome_message.value)
-    seconds = int(seconds) + 1
-    welcome_message.value = secondsToText(seconds)
+def incrementTime(byAmount, stopAt):
+    seconds = textToSeconds(timer.value)
+    print(seconds)
+    seconds = int(seconds) + byAmount
 
-app = App(title="Hello world", width=1000)
+    if seconds >= stopAt:
+        seconds = stopAt
+        timer.cancel(incrementTime)
+
+    timer.value = secondsToText(seconds)
+
+def decrementTime(byAmount, stopAt):
+    seconds = textToSeconds(timer.value)
+    print(seconds)
+    seconds = int(seconds) - byAmount
+
+    if seconds <= stopAt:
+        seconds = stopAt
+        timer.cancel(decrementTime)
+
+    timer.value = secondsToText(seconds)
+
+def beginCountdown():
+    timer.repeat(5, decrementTime, args=[1, 0])
+
+def beginCountup():
+    stopAt = textToSeconds(timer.value)
+    timer.value = secondsToText(0)
+    timer.repeat(5, incrementTime, args=[1, stopAt])
+
+
+
+
+
+
+
+app = App(title="Winter swimming clock", width=1000, height=600)
 
 displayClock = "00:00"
 timer = 0
 
-welcome_message = Text(app, text=displayClock, size=40, font="Ariel", color="lightblue")
-my_name = TextBox(app, width=30)
-update_text = PushButton(app, command=say_my_name, text="Display my name")
-text_size = Slider(app, command=change_text_size, start=10, end=80)
-
-welcome_message.repeat(50, incrementTime, args=[timer])
+timer = Text(app, text=displayClock, size=250, font="Courier New")
+add_time = PushButton(app, text="Dodaj", command=incrementTime, args=[15, 600], width=30, image="icon_plus.png")
+remove_time = PushButton(app, text="Odejmij", command=decrementTime, args=[15, 0], width=30, image="icon_minus.png")
+button_start_down = PushButton(app, text="Liczenie w dół", command=beginCountdown, width=30, image="icon_down.png")
+button_start_up = PushButton(app, text="Liczenie w górę", command=beginCountup, width=30, image="icon_up.png")
 
 app.display()
 
